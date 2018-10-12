7f3873f4cf86837506c2219c050f8b72b240b3cf
vasista <vasista@inzyte.com>
2018-10-12 23:31:31 +0530

    methods and variable changes and docstrings attached

--- rlsa.py
+++ rlsa.py
@@ -1,39 +1,54 @@
 from typing import List
 import numpy
 
-def rlsa(image: numpy.ndarray, horizontal: bool = True, vertical: bool = True, value: int = 0) -> numpy.ndarray:
-    # image must be binary
-    if isinstance(image, numpy.ndarray):
-        value = value if value>=0 else 0
+def iteration(image: numpy.ndarray, value: int) -> numpy.ndarray:
+    """
+    This method iterates over the provided image by converting 255's to 0's if the number of consecutive 255's are
+    less the "value" provided
+    """
+
+    rows, cols = image.shape
+    for row in range(0,rows):
+        count = 0
         try:
-            X, Y = image.shape
+            start = list(image[row:]).index(0) # to start the conversion from the 0 pixel
+        except ValueError:
+            start = 0
+        for col in range(start, cols):
+            if image[row, col] == 0:
+                if (col-count) < value:
+                    image[row, count:col] = 0               
+                count = col  
+    return image 
 
-            # RUN LENGTH SMOOTHING ALGORITHM working horizontal on the image
+def rlsa(image: numpy.ndarray, horizontal: bool = True, vertical: bool = True, value: int = 0) -> numpy.ndarray:
+    """
+    rlsa(RUN LENGTH SMOOTHING ALGORITHM) is to extract the block-of-text or the Region-of-interest(ROI) from the
+    document binary Image provided. Must pass binary image of ndarray type.
+    """
+    
+    if isinstance(image, numpy.ndarray): # image must be binary of ndarray type
+        value = value if value>=0 else 0 # consecutive pixel position checker value to convert 255 to 0
+        try:
+            # RUN LENGTH SMOOTHING ALGORITHM working horizontally on the image
             if horizontal:
-                for i in range(0,X):
-                    c = 0
-                    for j in range(0, Y):
-                        if image[i, j] == 0:
-                            if (j-c) < value:
-                                image[i, c:j] = 0               
-                            c = j    
+                image = iteration(image, value)   
 
-            # RUN LENGTH SMOOTHING ALGORITHM working vertical on the image
+            # RUN LENGTH SMOOTHING ALGORITHM working vertically on the image
             if vertical:
-                for i in range(0, Y):
-                    c = 0
-                    for j in range(0, X):
-                        if image[j, i] == 0:
-                            if (j-c) < value:
-                                image[c:j, i] = 0
-                            c = j
+                image = image.T
+                image = iteration(image, value)
+                image = image.T
 
         except (AttributeError, ValueError) as e:
+            image = None
             print("ERROR:\n")
-            print('Image must be an numpy array and must be in "binary". Use Opencv/PIL to convert the image to binary.\n')
+            print('Image must be an numpy ndarray and must be in "binary". Use Opencv/PIL to convert the image to binary.\n')
             print("import cv2;\nimage=cv2.imread('path_of_the_image');\ngray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY);\n\
                 (thresh, image_binary) = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)\n")
-            print("pass like this -- rlsa.rlsa(True, False, image_binary, 10)")
+            print("method usage -- rlsa.rlsa(image_binary, True, False, 10)")
     else:
-        print('Image must be an numpy array and must be in binary')
+        print('Image must be an numpy ndarray and must be in binary')
+        image = None
     return image
+
